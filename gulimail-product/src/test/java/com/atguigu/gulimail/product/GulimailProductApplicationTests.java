76fce2315a4f629e65124a9d4bbdf18356077d9e
zhiwuli <398147488@qq.com>
2024-05-23 23:53:46 +0800

    spring-cloud-alibaba引入，nacos引入

--- gulimail-product/src/test/java/com/atguigu/gulimail/product/GulimailProductApplicationTests.java
+++ gulimail-product/src/test/java/com/atguigu/gulimail/product/GulimailProductApplicationTests.java
@@ -1,13 +1,24 @@
 package com.atguigu.gulimail.product;
 
+import com.atguigu.gulimail.product.entity.BrandEntity;
+import com.atguigu.gulimail.product.service.BrandService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GulimailProductApplicationTests {
 
+    @Autowired
+    private BrandService brandService;
     @Test
     void contextLoads() {
+
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setName("xiaomi");
+        boolean save = brandService.save(brandEntity);
+        Assertions.assertTrue(save);
     }
 
 }
